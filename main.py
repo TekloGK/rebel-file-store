502d799399816e5e998ef564646b317e179fea53
TekloGK <154121704+TekloGK@users.noreply.github.com>
2024-10-21 23:36:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+import asyncio
+import warnings
 from bot import bot
+from handler import logger
+import time
 
 
-bot.run()
+bot_loop = asyncio.get_event_loop()
+
+def main_func():
+    async def start_services():
+        global bot, bot_loop
+        await bot.start() 
+          
+    bot_loop.run_until_complete(start_services())
+    bot_loop.run_forever()
+	
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore", message="There is no current event loop")
+    main_func()
